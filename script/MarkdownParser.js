7f53568ee219a624fa2248e16b08ce9f4902a6b5
yongruifang <yongruifang@163.com>
2023-10-26 14:45:53 +0800

    修正

--- script/MarkdownParser.js
+++ script/MarkdownParser.js
@@ -1,11 +1,10 @@
-var HtmlHandler = /** @class */ (function () {
-    function HtmlHandler() {
-    }
-    HtmlHandler.prototype.TextChangeHandler = function (id, output) {
-        var markdownSource = document.getElementById(id);
-        var htmlOutput = document.getElementById(output);
+"use strict";
+class HtmlHandler {
+    TextChangeHandler(id, output) {
+        let markdownSource = document.getElementById(id);
+        let htmlOutput = document.getElementById(output);
         if (markdownSource !== null) {
-            markdownSource.onkeyup = function (e) {
+            markdownSource.onkeyup = (e) => {
                 console.debug(e, markdownSource.value);
                 if (markdownSource.value) {
                     htmlOutput.innerHTML = markdownSource.value;
@@ -15,6 +14,6 @@ var HtmlHandler = /** @class */ (function () {
                 }
             };
         }
-    };
-    return HtmlHandler;
-}());
+    }
+}
+//# sourceMappingURL=MarkdownParser.js.map
